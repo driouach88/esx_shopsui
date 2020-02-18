e4f6b457f49a538d5e00975c7bf435589dc6ae70
NeQYT <46975849+NeQYT@users.noreply.github.com>
2020-02-18 14:30:26 +0100

    Add files via upload

---
+++ esx_weaponshop/config.lua
@@ -0,0 +1,30 @@
+Config               = {}
+
+Config.DrawDistance  = 100
+Config.Size          = { x = 1.5, y = 1.5, z = 0.5 }
+Config.Color         = { r = 0, g = 155, b = 253 }
+Config.Type          = 23
+
+Config.Locale        = 'pl'
+
+Config.LicenseEnable = true -- only turn this on if you are using esx_license
+Config.LicensePrice  = 5000
+
+Config.Zones = {
+
+	GunShop = {
+		Legal = true,
+		Items = {},
+		Locations = {
+			vector3(22.0, -1107.2, 28.8)
+		}
+	},
+
+	BlackWeashop = {
+		Legal = false,
+		Items = {},
+		Locations = {
+			vector3(728.92, 4188.70, 39.75)
+		}
+	}
+}
